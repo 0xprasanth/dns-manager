67de2c3f1e91260b628be97ea5615c109bc3c875
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-05-09 17:57:22 +0530

    login and signup axios request logic

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useAuthContext } from "@/hooks/useAuthContext";
+
+const useAuth = () => {
+  const { handleLoggedIn } = useAuthContext();
+
+  const signup = async (email, password, username) => {
+    try{
+        const response = await axios.post(
+            `${import.meta.env.VITE_API_URL}/signup`,
+            {
+                email,
+                password,
+                username
+            }
+        );
+        const token = response.data.accesstoken;
+        Cookies.set("token", token, { expires: 7 });
+        handleLoggedIn(true);
+
+    }catch (err) {
+        console.log(err);
+    }
+  }
+
+  const login = async (email, password) => {
+    try{
+        const response = await axios.post(
+            `${import.meta.env.VITE_API_URL}/login`,
+            {
+              email,
+              password,
+            }
+          );
+
+          const token = response.data.acesstoken;
+          Cookies.set("token", token, {expires: 7})
+          handleLoggedIn(true);
+
+          return response.data.data;
+    }catch(err){
+        console.log(err);
+    }
+  }
+  return { signup, login };
+};
+
+export default useAuth;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,27 +1,62 @@
-import React from "react";
 import { toast } from "sonner";
-import { Row, Container, Col } from "react-bootstrap";
-import { Card, Form, FloatingLabel } from "react-bootstrap";
+import React from "react";
+import { Row, Container, Col, CardBody } from "react-bootstrap";
+import { Card, Form, FloatingLabel, CardHeader, CardTitle } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  return (
-    <div className="">
-        <Card>
+    const {login } = useAuth();
+    const navigate = useNavigate();
 
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Email address"
-        className="mb-3"
-        >
-        <Form.Control type="email" placeholder="name@example.com" />
-      </FloatingLabel>
-      
-      <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
-      </FloatingLabel>
-      
-      <button onClick={() => toast("Logging IN")}>Submit</button>
-          </Card>
+    const [values, setValues] = useState<{ email, password }>({
+      email: "",
+      password: "",
+    });
+  
+    const handleChange =
+      (prop) => (e) => {
+        setValues({ ...values, [prop]: e.target.value });
+      };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      if (!values.email || !values.password) {
+        return;
+      }
+  
+      await login(values.email, values.password);
+  
+      toast.success("Loggedin successfully");
+  
+      navigate("/dashboard");
+    };
+
+  return (
+    <div className="absolute inset-0 flex items-center justify-center flex-col gap-4">
+        <div className="w-full p-2 text-2xl bg-slate-200/20 shadow-sm font-bold tracking-tighter capitalize fixed top-0 flex items-start">
+            DNS Manager
+        </div>
+        <CardHeader className="w-full max-w-sm">
+            <CardTitle className="text-2xl"> Login</CardTitle>
+            <CardBody>Enter Email and Password</CardBody>
+        </CardHeader>
+        <form onSubmit={handleSubmit}>
+            <CardBody className="grid gap-4">
+                <div className="grid gap-2">
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        value={values.email}
+                        // onChange={handleChange("email")}
+                    />
+                </div>
+            </CardBody>
+        </form>
+        
+
     </div>
   );
 };
