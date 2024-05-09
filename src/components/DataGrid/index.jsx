50ef1ce59a65801bef09e335dea5626d4f28824f
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-05-09 22:20:39 +0530

    DataGrid component init

---
+++ src/components/DataGrid/index.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const DataGrid = () => {
+  return (
+    <div>DataGrid</div>
+  )
+}
+
+export default DataGrid
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Container from '../layouts/Container'
+import DataGrid from '../components/DataGrid/index'
 
 const Dashboard = () => {
   return (
     <>
         <Container>
-            DashBoard
+            <DataGrid />
         </Container>
     </>
   )
